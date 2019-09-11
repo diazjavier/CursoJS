a06ea2eddeb6a3736fb877e3ddcc472e8e90f886
diazjavier <54905923+diazjavier@users.noreply.github.com>
2019-09-11 16:50:20 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,10 +1,31 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
-alert (mesDelAño);
-
+//alert (mes);
+switch(mes){
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+        alert("Falta para el invierno");
+        break;
+    case "Julio":
+    case "Agosto":
+        alert("Abrigate que hace frio");
+        break;
+    case "Septiembre":
+    case "Octubre":
+    case "Diciembre":
+        alert("Ya pasamos el frio, ahora calor!!!");
+        break;
+    default:
+        alert("Es Noviembre");
+                             
+}
 
 
 
